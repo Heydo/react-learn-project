2f6fd0e9f60b001d80f14409294e489da7c038bc
Heydo <heydo96@gmail.com>
2023-06-26 22:16:16 +0800

    第一个空白的React项目
    通过create-react-app脚手架创建，并删除了多余的文件，只保留最基础的结构。

---
+++ 1-demo/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <div>第一个空白的React项目</div>
+  </React.StrictMode>
+);
