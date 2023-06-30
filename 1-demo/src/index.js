e5fd3fea6b5fdb8f8a91f8316d03c3501317afd5
Heydo <heydo96@gmail.com>
2023-06-30 15:01:45 +0800

    修改浏览器兼容；
    1. 会对postcss-loader生效：控制CSS3前缀
    2. 对babel-loader生效：控制ES6代码转换
    遗留问题：无法处理ES6内置api的兼容。需要@babel/polyfill的帮助，它对常见的内置api进行的重写。
    处理方法：
    - 安装@babel/polyfill，在入口中引入；
    - 但是在脚手架中不需要自己安装，自带react-app-polyfill（对@babel/polyfill的重写），但还是需要在入口文件最开头引入

--- 1-demo/src/index.js
+++ 1-demo/src/index.js
@@ -1,3 +1,10 @@
+// 对内置ES6 api 做兼容处理
+// 想兼容谁就导入谁
+import 'react-app-polyfill/ie9'
+import 'react-app-polyfill/ie11'
+// stable是必须要的
+import "react-app-polyfill/stable"
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '@/index.less'
